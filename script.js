ec2a7bbf7e54ef4605000a2a9f858419fbae99f7
OczkowiczMonika <47368207+OczkowiczMonika@users.noreply.github.com>
2019-03-17 21:10:20 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ let key2 = new Audio('https://sampleswap.org/samples-ghost/DRUMS%20(FULL%20KITS)
 let key3 = new Audio('https://sampleswap.org/samples-ghost/MELODIC%20LOOPS/KEYS%20ORGAN%20PIANO%20LOOPS/919[kb]090_hard-high-piano-arpeggio.wav.mp3');
 
 
+var audio1;
+
 
 function removeTransition(e) {
 
@@ -16,9 +18,9 @@ function removeTransition(e) {
 
 function playSound(e) {
 
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const audio = document.querySelector(`audio[data-key="${e.key}"]`);
 
-    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`div[data-key="${e.key}"]`);
 
     if (!audio) return;
 
@@ -36,184 +38,239 @@ keys.forEach(key => key.addEventListener('transitionend', removeTransition));
 
 window.addEventListener('keydown', playSound);
 
+let record1 = document.getElementById('RECORD1');
+
+let record2 = document.getElementById('RECORD2');
+
+let record3 = document.getElementById('RECORD3');
+
+let record4 = document.getElementById('RECORD4');
+
+let stop1 = document.getElementById('STOP1');
+
+let stop2 = document.getElementById('STOP2');
+
+let stop3 = document.getElementById('STOP3');
+
+let stop4 = document.getElementById('STOP4');
+
+let play1 = document.getElementById('PLAY1');
+
+let play2 = document.getElementById('PLAY2');
+
+let play3 = document.getElementById('PLAY3');
+
+let play4 = document.getElementById('PLAY4');
 
 
 
-record1.addEventListener(`click`, function () {
+
+
+record1.addEventListener('click', function () {
 
     navigator.mediaDevices.getUserMedia({ audio: true })
 
         .then(stream => {
 
-            const mediaRecorder = new MediaRecorder(stream);
+            mediaRecorder = new MediaRecorder(stream);
 
             mediaRecorder.start();
 
             const audioChunks = [];
 
-            mediaRecorder.addEventListener(`dataavailable`, (event) => {
+            mediaRecorder.addEventListener('dataavailable', (event) => {
 
                 audioChunks.push(event.data);
 
             });
 
+			var audio;
 
 
-
-            mediaRecorder.addEventListener(`stop`, () => {
-
-
+			mediaRecorder.addEventListener('stop', () => {
 
                 const audioBlob = new Blob(audioChunks);
 
                 const audioUrl = URL.createObjectURL(audioBlob);
 
-                const audio = new Audio(audioUrl);
+                audio = new Audio(audioUrl);
+
+			});
+
+			stop1.addEventListener('click', () => {
+
+				mediaRecorder.stop();
+
+			});
 
 
+            play1.addEventListener('click', () => {
 
-                play1.addEventListener(`click`, function () {
+                audio.play();
 
-                    audio.play();
-
-
-
-                });
-
-            });
-
-
-
-
-            stop1.addEventListener(`click`, () => {
-
-                mediaRecorder.stop();
-
-            })
+			});
 
 
 
         });
 
 
-
 });
 
-record2.addEventListener(`click`, function () {
+record2.addEventListener('click', function () {
 
     navigator.mediaDevices.getUserMedia({ audio: true })
 
         .then(stream => {
 
-            const mediaRecorder = new MediaRecorder(stream);
+            mediaRecorder = new MediaRecorder(stream);
 
             mediaRecorder.start();
 
             const audioChunks = [];
 
-            mediaRecorder.addEventListener(`dataavailable`, (event) => {
+            mediaRecorder.addEventListener('dataavailable', (event) => {
 
                 audioChunks.push(event.data);
 
             });
 
+			var audio;
 
 
-
-            mediaRecorder.addEventListener(`stop`, () => {
-
-
+			mediaRecorder.addEventListener('stop', () => {
 
                 const audioBlob = new Blob(audioChunks);
 
                 const audioUrl = URL.createObjectURL(audioBlob);
 
-                const audio = new Audio(audioUrl);
+                audio = new Audio(audioUrl);
+
+			});
+
+			stop2.addEventListener('click', () => {
+
+				mediaRecorder.stop();
+
+			});
 
 
+            play2.addEventListener('click', () => {
 
-                play2.addEventListener(`click`, function () {
+                audio.play();
 
-                    audio.play();
-
-
-
-                });
-
-            });
-
-
-
-
-            stop2.addEventListener(`click`, () => {
-
-                mediaRecorder.stop();
-
-            })
+			});
 
 
 
         });
 
 
-
 });
-record3.addEventListener(`click`, function () {
+
+record3.addEventListener('click', function () {
 
     navigator.mediaDevices.getUserMedia({ audio: true })
 
         .then(stream => {
 
-            const mediaRecorder = new MediaRecorder(stream);
+            mediaRecorder = new MediaRecorder(stream);
 
             mediaRecorder.start();
 
             const audioChunks = [];
 
-            mediaRecorder.addEventListener(`dataavailable`, (event) => {
+            mediaRecorder.addEventListener('dataavailable', (event) => {
 
                 audioChunks.push(event.data);
 
             });
 
+			var audio;
 
 
-
-            mediaRecorder.addEventListener(`stop`, () => {
-
-
+			mediaRecorder.addEventListener('stop', () => {
 
                 const audioBlob = new Blob(audioChunks);
 
                 const audioUrl = URL.createObjectURL(audioBlob);
 
-                const audio = new Audio(audioUrl);
+                audio = new Audio(audioUrl);
+
+			});
+
+			stop3.addEventListener('click', () => {
+
+				mediaRecorder.stop();
+
+			});
 
 
+            play3.addEventListener('click', () => {
 
-                play3.addEventListener(`click`, function () {
+                audio.play();
 
-                    audio.play();
-
-
-
-                });
-
-            });
-
-
-
-
-            stop3.addEventListener(`click`, () => {
-
-                mediaRecorder.stop();
-
-            })
+			});
 
 
 
         });
 
 
+});
+
+record4.addEventListener('click', function () {
+
+    navigator.mediaDevices.getUserMedia({ audio: true })
+
+        .then(stream => {
+
+            mediaRecorder = new MediaRecorder(stream);
+
+            mediaRecorder.start();
+
+            const audioChunks = [];
+
+            mediaRecorder.addEventListener('dataavailable', (event) => {
+
+                audioChunks.push(event.data);
+
+            });
+
+			var audio;
+
+
+			mediaRecorder.addEventListener('stop', () => {
+
+                const audioBlob = new Blob(audioChunks);
+
+                const audioUrl = URL.createObjectURL(audioBlob);
+
+                audio = new Audio(audioUrl);
+
+			});
+
+			stop4.addEventListener('click', () => {
+
+				mediaRecorder.stop();
+
+			});
+
+
+            play4.addEventListener('click', () => {
+
+                audio.play();
+
+			});
+
+
+
+        });
+
 
 });
+
+
+
+				
+
